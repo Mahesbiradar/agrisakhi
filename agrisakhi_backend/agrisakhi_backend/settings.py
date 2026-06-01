d6965cf4b7493f8fe55e24e0fb7f492b1532cbb6
mahesbiradar <maheshbiradar103@gmail.com>
2026-06-02 01:26:04 +0530

    Ready for deployment

--- agrisakhi_backend/agrisakhi_backend/settings.py
+++ agrisakhi_backend/agrisakhi_backend/settings.py
@@ -62,12 +62,17 @@ WSGI_APPLICATION = 'agrisakhi_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'agrisakhi'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.environ.get('DB_NAME', 'agrisakhi'),
+        # 'USER': os.environ.get('DB_USER', 'postgres'),
+        # 'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        # 'HOST': os.environ.get('DB_HOST', 'localhost'),
+        # 'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
